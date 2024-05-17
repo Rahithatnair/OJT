9966a4f6a2b27700aac52ba567f8ac6025e498a2
Rahithatnair <122440851+Rahithatnair@users.noreply.github.com>
2024-05-17 14:25:31 +0530

    Delete DAY2/dictionary.py

--- DAY2/dictionary.py
+++
@@ -1,8 +0,0 @@
-
-# Iterate over keys and values in a dictionary.
-
-dictionary={'name':"Raahitha",'age':22,'place':"venjaramoodu"}
-for key, value in  dictionary.items():
-    print(key, value)
-    
-    
