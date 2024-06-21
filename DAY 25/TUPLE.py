921039a8b5c3ddbcc2ece55332c3c4680e9e33cb
Rahithatnair <122440851+Rahithatnair@users.noreply.github.com>
2024-06-21 14:08:58 +0530

    Add files via upload

---
+++ DAY 25/TUPLE.py
@@ -0,0 +1,17 @@
+
+# The + operator, concatenation operator is used to joining two tuples
+
+tuplel = (1, 2, 3)
+tuple2 =(4,5,3,6)
+print (tuplel + tuple2)
+
+tuplel = (1,2,3)
+tuple2 =(4,5,6)
+tuple3 =('a', 12.5, (7,8))
+print (tuplel + tuple2 + tuple3)
+
+# You can use * operator to replicate a tuple a specified number of times
+
+tuplel =(1,2,3)
+print(tuplel *4)
+
